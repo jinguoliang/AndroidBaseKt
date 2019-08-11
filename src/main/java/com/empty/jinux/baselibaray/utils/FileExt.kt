622b873cf5ea32836386d8752ce5a30d5a8b61e6
jinux <jinguoliang@foundao.com>
2019-08-11 14:49:39 +0800

    add method to compute a file size

---
+++ src/main/java/com/empty/jinux/baselibaray/utils/FileExt.kt
@@ -0,0 +1,16 @@
+package com.empty.jinux.baselibaray.utils
+
+import java.io.File
+
+fun File.getFileSize(excludes: (File) -> Boolean): Long {
+    if (excludes(this)) {
+        return 0
+    }
+    when {
+        isFile -> return length()
+        isDirectory -> return listFiles().fold(0L) { acc, f ->
+            return acc + f.getFileSize(excludes)
+        }
+        else -> return 0
+    }
+}
